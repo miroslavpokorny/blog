b2a93671a148ee7161992c1489873d453ea7b0c0
Miroslav Pokorný <miroslavpokorny94@gmail.com>
2018-01-08 20:04:13 +0100

    Added prettier tool

--- src/blog-admin/src/api/Endpoint.ts
+++ src/blog-admin/src/api/Endpoint.ts
@@ -1,8 +1,9 @@
 export enum Endpoint {
-    SignIn = '/api/sign/in',
-    SignOut = '/api/sign/out',
-    GetLoggedUser = '/api/getLoggedUser',
-    SignUp = '/api/sign/up',
-    ProfileLoad = '/api/profile/load',
-    ProfileEdit = '/api/profile/edit',
+    SignIn = "/api/sign/in",
+    SignOut = "/api/sign/out",
+    GetLoggedUser = "/api/getLoggedUser",
+    SignUp = "/api/sign/up",
+    ProfileLoad = "/api/profile/load",
+    ProfileEdit = "/api/profile/edit",
+    UsersList = "/api/users/list"
 }
